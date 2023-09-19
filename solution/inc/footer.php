97092db8186117e661d733f55c81e44f964d3695
TeacherArea <sewilius.berg@lbs.se>
2023-09-19 12:54:19 +0200

    first commit, initializing project

--- solution/inc/footer.php
+++ solution/inc/footer.php
@@ -5,9 +5,9 @@
   <div id="date"></div>
   <script>
     const date = new Date();
-    document.getElementById('date').innerHTML = '&copyinitialize.se ' + date.getFullYear();
+    document.getElementById('date').innerHTML = '&copy Traversy Media ' + date.getFullYear();
   </script>
-  <a href="https://www.youtube.com/watch?v=BUCiSSyIGGU">Länk till video</a>
+  <a href="https://www.youtube.com/watch?v=BUCiSSyIGGU">Länk till video</a> (3:15 min)
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
